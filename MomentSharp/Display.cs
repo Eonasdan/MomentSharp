c57dfa2980dfda9033cdfe476ad73aa66994bf9d
Eonasdan <jonpet2@gmail.com>
2015-08-11 11:28:03 -0400

    rename local to language for clarity

--- MomentSharp/Display.cs
+++ MomentSharp/Display.cs
@@ -19,7 +19,7 @@ namespace MomentSharp
         /// <returns>Localized string e.g. 4 years ago</returns>
         public static string From(this Moment moment, DateTime dateTime = default(DateTime), bool showSuffix = true)
         {
-            var local = moment.Language;
+            var language = moment.Language;
             if (dateTime == default(DateTime)) dateTime = DateTime.UtcNow;
 
             var timeSpan = (moment.DateTime() - dateTime);
@@ -29,54 +29,54 @@ namespace MomentSharp
             {
                 if (timeSpan.TotalSeconds.InRange(0, 45)) // 0-45 seconds
                 {
-                    return local.Translate(RelativeTime.Seconds, timeSpan.Seconds, showSuffix, isFuture);
+                    return language.Translate(RelativeTime.Seconds, timeSpan.Seconds, showSuffix, isFuture);
                 }
                 if (timeSpan.TotalSeconds.InRange(46, 90)) // 46-90 seconds
                 {
-                    return local.Translate(RelativeTime.Minute, timeSpan.Minutes, showSuffix, isFuture);
+                    return language.Translate(RelativeTime.Minute, timeSpan.Minutes, showSuffix, isFuture);
                 }
                 if (timeSpan.TotalSeconds.InRange(91, 60*45)) //91 seconds to 45 minutes
                 {
-                    return local.Translate(RelativeTime.Minutes, timeSpan.Minutes, showSuffix, isFuture);
+                    return language.Translate(RelativeTime.Minutes, timeSpan.Minutes, showSuffix, isFuture);
                 }
             }
             if (timeSpan.TotalMinutes < 60*22)
             {
                 if (timeSpan.TotalMinutes.InRange(46, 90)) //46 minutes to 90 minutes
                 {
-                    return local.Translate(RelativeTime.Hour, timeSpan.Hours, showSuffix, isFuture);
+                    return language.Translate(RelativeTime.Hour, timeSpan.Hours, showSuffix, isFuture);
                 }
                 if (timeSpan.TotalMinutes.InRange(91, 60*22)) //91 minutes to 22 hours
                 {
-                    return local.Translate(RelativeTime.Hours, timeSpan.Hours, showSuffix, isFuture);
+                    return language.Translate(RelativeTime.Hours, timeSpan.Hours, showSuffix, isFuture);
                 }
             }
             if (timeSpan.TotalHours < 24*25)
             {
                 if (timeSpan.TotalHours.InRange(23, 36)) //23-36 hours
                 {
-                    return local.Translate(RelativeTime.Day, timeSpan.Days, showSuffix, isFuture);
+                    return language.Translate(RelativeTime.Day, timeSpan.Days, showSuffix, isFuture);
                 }
                 if (timeSpan.TotalHours.InRange(37, 24*25)) //37 hours to 25 days
                 {
-                    return local.Translate(RelativeTime.Days, timeSpan.Days, showSuffix, isFuture);
+                    return language.Translate(RelativeTime.Days, timeSpan.Days, showSuffix, isFuture);
                 }
             }
             if (timeSpan.TotalDays.InRange(26, 45)) //26-45 days
             {
-                return local.Translate(RelativeTime.Month, 1, showSuffix, isFuture);
+                return language.Translate(RelativeTime.Month, 1, showSuffix, isFuture);
             }
             if (timeSpan.TotalDays.InRange(46, 345)) // 46-345 days
             {
-                return local.Translate(RelativeTime.Months, (int) Math.Abs(timeSpan.Days/30.4), showSuffix, isFuture);
+                return language.Translate(RelativeTime.Months, (int) Math.Abs(timeSpan.Days/30.4), showSuffix, isFuture);
             }
             if (timeSpan.TotalDays.InRange(346, 547)) //346-547 days (1.5 years)
             {
-                return local.Translate(RelativeTime.Year, 1, showSuffix, isFuture);
+                return language.Translate(RelativeTime.Year, 1, showSuffix, isFuture);
             }
             if (timeSpan.TotalDays.InRange(548, 7305))
             {
-                return local.Translate(RelativeTime.Years, Math.Abs(timeSpan.Days/365), showSuffix, isFuture);
+                return language.Translate(RelativeTime.Years, Math.Abs(timeSpan.Days/365), showSuffix, isFuture);
             }
             throw new Exception("Couldn't find an exceptable range to return");
         }
@@ -91,19 +91,19 @@ namespace MomentSharp
         /// <returns>Localized string</returns>
         public static string Calendar(this Moment moment, DateTime dateTime = default(DateTime))
         {
-            var local = moment.Language;
+            var language = moment.Language;
             if (dateTime == default(DateTime)) dateTime = DateTime.Now;
 
-            //compare width DateTimeParts.Day
+            //compare with DateTimeParts.Day
             var timeSpan = (moment.DateTime() - dateTime.StartOf(DateTimeParts.Day)).TotalDays;
 
-            if (timeSpan < -6) return local.Translate(Globalization.Calendar.SameElse, moment.LocalTime());
-            if (timeSpan < -1) return local.Translate(Globalization.Calendar.LastWeek, moment.LocalTime());
-            if (timeSpan < 0) return local.Translate(Globalization.Calendar.LastDay, moment.LocalTime());
-            if (timeSpan < 1) return local.Translate(Globalization.Calendar.SameDay, moment.LocalTime());
-            if (timeSpan < 2) return local.Translate(Globalization.Calendar.NextDay, moment.LocalTime());
-            if (timeSpan < 7) return local.Translate(Globalization.Calendar.NextWeek, moment.LocalTime());
-            return local.Translate(Globalization.Calendar.SameElse, moment.LocalTime());
+            if (timeSpan < -6) return language.Translate(Globalization.Calendar.SameElse, moment.LocalTime());
+            if (timeSpan < -1) return language.Translate(Globalization.Calendar.LastWeek, moment.LocalTime());
+            if (timeSpan < 0) return language.Translate(Globalization.Calendar.LastDay, moment.LocalTime());
+            if (timeSpan < 1) return language.Translate(Globalization.Calendar.SameDay, moment.LocalTime());
+            if (timeSpan < 2) return language.Translate(Globalization.Calendar.NextDay, moment.LocalTime());
+            if (timeSpan < 7) return language.Translate(Globalization.Calendar.NextWeek, moment.LocalTime());
+            return language.Translate(Globalization.Calendar.SameElse, moment.LocalTime());
         }
 
         /// <summary>
